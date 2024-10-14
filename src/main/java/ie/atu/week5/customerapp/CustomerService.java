a377c43c2c603f2457484c1ada47680955649ba6
Dan Koskiranta <G00397054@atu.ie>
2024-10-14 14:50:14 +0100

    Added GlobalException and service classes

---
+++ src/main/java/ie/atu/week5/customerapp/CustomerService.java
@@ -0,0 +1,35 @@
+package ie.atu.week5.customerapp;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    CustomerRepository customerRepository;
+    OrderRepository orderRepository;
+
+    private List<Customer>customerList = new ArrayList<>();
+
+
+    public CustomerService(CustomerRepository customerRepository, OrderRepository orderRepository) {
+        this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Customer>createCustomer(Customer customer){
+        customerRepository.save(customer);
+        return customerRepository.findAll();
+    }
+
+    public List<Customer>deleteCustomer(String id){
+
+        if(customerRepository.existsById(id)){
+            customerRepository.deleteById(id);
+        }
+        return customerRepository.findAll();
+    }
+}
---
+++ src/main/java/ie/atu/week5/customerapp/OrderService.java
@@ -0,0 +1,36 @@
+package ie.atu.week5.customerapp;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    OrderRepository orderRepository;
+
+    List<Order> orders = new ArrayList<>();
+
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> getOrdersByCustomerId(String customerId){
+        orderRepository.findByCustomerId(customerId);
+        return orderRepository.findAll();
+    }
+
+    public List<Order> createOrder(Order order){
+        orderRepository.save(order);
+        return orderRepository.findAll();
+    }
+
+    public List<Order> deleteOrder(String id){
+        if(orderRepository.existsById(id)){
+            orderRepository.deleteById(id);
+        }
+        return orderRepository.findAll();
+    }
+}
