81cb246c5f9c83a6ef8997a9b55f3488793ebb93
Dan Koskiranta <G00397054@atu.ie>
2024-10-16 20:39:40 +0100

    Modified service and controller classes

--- src/main/java/ie/atu/week5/customerapp/CustomerController.java
+++ src/main/java/ie/atu/week5/customerapp/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,9 +14,13 @@ import java.util.Optional;
 public class CustomerController {
 
     private final CustomerRepository customerRepository;
+    private final CustomerService customerService;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    List <Customer> customers = new ArrayList<>();
+
+    public CustomerController(CustomerRepository customerRepository, CustomerService customerService) {
         this.customerRepository = customerRepository;
+        this.customerService = customerService;
     }
 
     @GetMapping
@@ -23,6 +28,30 @@ public class CustomerController {
         return customerRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Customer>> getCustomerById(@PathVariable String id) {
+        customers = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customers);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<Customer>> createCustomer(@Valid @RequestBody Customer customer) {
+        customers = customerService.createCustomer(customer);
+        return ResponseEntity.ok(customers);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<Customer>> deleteCustomer(@PathVariable String id) {
+        customers = customerService.deleteCustomer(id);
+        return ResponseEntity.ok(customers);
+    }
+}
+
+
+
+//Original
+
+/*
     @GetMapping("/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable String id) {
         Optional<Customer> customer = customerRepository.findById(id);
@@ -46,5 +75,4 @@ public class CustomerController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
-    }
-}
+    }*/
